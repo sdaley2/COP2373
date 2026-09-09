d5a6810150c09c4fd0c83e22e75f269300cbf984
Summer Daley <sdaley2@student.scf.edu>
2026-09-09 09:02:53 -0400

    I think I got it...

--- testchanges.py
+++ testchanges.py
@@ -1,6 +1,6 @@
 print("Testing for practice")
 print("I don't know what I am doing!!!")
-
+print("I think I am getting it now.")
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
